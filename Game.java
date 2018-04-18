2e85d6a89073cbc20b61f1857e62e53785f91aa2
Max Chehab <maxchehab@gmail.com>
2018-04-17 19:00:38 -0700

    Implemented multiple page design. Dummy home and help page

--- Game.java
+++ Game.java
@@ -4,13 +4,11 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.HashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -21,8 +19,8 @@ public class Game {
 
     public static void main(String[] args) {
 
-        StateManager.addState("home", new State(new Panel("home")));
-        StateManager.addState("test", new State(new Panel("test")));
+        StateManager.addState("HomePage", new State(new HomePage()));
+        StateManager.addState("HelpPage", new State(new HelpPage()));
 
         StateManager.addStateChangedListener(new StateChangedListener() {
             @Override
@@ -31,63 +29,13 @@ public class Game {
                 frame.getContentPane().add(newState.getPanel());
                 frame.revalidate();
                 frame.pack();
+                frame.setSize(1200, 800);
             }
         });
 
-        StateManager.changeState("home");
-
+        StateManager.changeState("HelpPage");
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-    }
-}
-
-class Panel extends JPanel {
-    static final long serialVersionUID = 0; // JFFrame requires a unique number.
-
-    private JLabel label;
-
-    public Panel(String labelName) {
-        super();
-        this.label = new JLabel(labelName);
-
-        // create a new panel with GridBagLayout manager
-
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor = GridBagConstraints.WEST;
-        constraints.insets = new Insets(10, 10, 10, 10);
-
-        // add components to the panel
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        this.add(label, constraints);
-
-        this.addMouseListener(new MouseListener() {
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                StateManager.changeState("test");
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
-
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
-            }
-        });
     }
 }
---
+++ HomePage.java
@@ -0,0 +1,64 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+class HomePage extends JPanel {
+    static final long serialVersionUID = 0; // JFFrame requires a unique number.
+
+    private JLabel label;
+
+    public HomePage() {
+        // super();
+        this.label = new JLabel("Home Page");
+
+        // create a new panel with GridBagLayout manager
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // add components to the panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        this.add(label, constraints);
+
+        this.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                StateManager.changeState("HelpPage");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+        });
+    }
+}
